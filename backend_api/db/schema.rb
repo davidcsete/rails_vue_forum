846d22faba401ae6162d349a3aa7ef992e294e4c
David Csete <davidcsetefogel@gmail.com>
2022-05-01 13:05:29 +0300

    Added docker to the project, still needs to be updated because it does not update the code inside the containers on code changes

--- backend_api/db/schema.rb
+++ backend_api/db/schema.rb
@@ -11,12 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_04_17_085641) do
-  create_table "articles", id: { type: :integer, unsigned: true }, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.text "title", null: false
-    t.text "content", null: false
-  end
-
-  create_table "jwt_denylist", charset: "utf8mb3", force: :cascade do |t|
+  create_table "jwt_denylist", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "jti", null: false
     t.datetime "exp", null: false
     t.datetime "created_at", null: false
@@ -24,7 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_17_085641) do
     t.index ["jti"], name: "index_jwt_denylist_on_jti"
   end
 
-  create_table "users", charset: "utf8mb3", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
--- vue_frontend/README.md
+++ vue_frontend/README.md
@@ -1,4 +1,4 @@
-# rails_vue_blog
+# vue_frontend
 
 This template should help get you started developing with Vue 3 in Vite.
 
